90416c52f1f28faa3892f15cdd451bd66a910113
Allan Stewart <allan-stewart@pluralsight.com>
2016-05-04 20:05:15 -0600

    FizzBuzz

---
+++ fizzbuzz/fizzbuzz.js
@@ -0,0 +1,56 @@
+module.exports = (input, option) => {
+  var algorithms = {
+    'basic': basic,
+    'noConditional': noConditional,
+    'reduce': reduce
+  };
+  var algorithm = algorithms[option] || basic
+  return algorithm(input);
+};
+
+var basic = input => {
+  var isMultipleOfThree = (input % 3 == 0);
+  var isMultipleOfFive = (input % 5 == 0);
+
+  if (isMultipleOfThree && isMultipleOfFive) {
+    return 'FizzBuzz';
+  }
+  if (isMultipleOfThree) {
+    return 'Fizz';
+  }
+  if (isMultipleOfFive) {
+    return 'Buzz';
+  }
+  return input;
+};
+
+var noConditional = input => {
+  var isMultipleOfThree = (input % 3 == 0);
+  var isMultipleOfFive = (input % 5 == 0);
+  var key = `${isMultipleOfThree}|${isMultipleOfFive}`;
+
+  var methods = {
+    'true|true': () => { return 'FizzBuzz' },
+    'true|false': () => { return 'Fizz' },
+    'false|true': () => { return 'Buzz' },
+    'false|false': () => { return input }
+  };
+
+  return methods[key]();
+};
+
+var reduce = input => {
+  var methods = [
+    x => { return (x % 3 == 0) ? 'Fizz' : null },
+    x => { return (x % 5 == 0) ? 'Buzz' : null },
+    x => { return x }
+  ];
+
+  return methods.reduce((prev, algorithm) => {
+    var current = algorithm(input);
+    if (prev != null && typeof current == typeof prev) {
+      return `${prev}${current}`;
+    }
+    return (typeof prev == 'string') ? prev : current;
+  }, null);
+};
---
+++ fizzbuzz/test/fizzbuzz.tests.js
@@ -0,0 +1,60 @@
+var fizzbuzz = require('../fizzbuzz.js');
+var assert = require('assert');
+
+describe('fizzbuzz', () => {
+  describe('basic', () => {
+    var test = (input, expected) => {
+      it(`prints ${expected} when given ${input}`, () => {
+        assert.equal(fizzbuzz(input), expected);
+      });
+    };
+
+    test(1, 1);
+    test(2, 2);
+    test(3, 'Fizz');
+    test(4, 4);
+    test(5, 'Buzz');
+    test(6, 'Fizz');
+    test(7, 7);
+    test(8, 8);
+    test(9, 'Fizz');
+    test(10, 'Buzz');
+    test(11, 11);
+    test(12, 'Fizz');
+    test(13, 13);
+    test(14, 14);
+    test(15, 'FizzBuzz');
+    test(16, 16);
+    test(17, 17);
+    test(18, 'Fizz');
+    test(19, 19);
+    test(20, 'Buzz');
+    test(21, 'Fizz');
+  });
+
+  describe('noConditional', () => {
+    var test = (input) => {
+      var expected = fizzbuzz(input);
+      it(`matches the basic output of ${expected} for ${input}`, () => {
+        assert.equal(fizzbuzz(input, 'noConditional'), expected);
+      });
+    };
+
+    for (var i = 1; i <= 100; i++) {
+      test(i);
+    }
+  });
+
+  describe('reduce', () => {
+    var test = (input) => {
+      var expected = fizzbuzz(input);
+      it(`matches the basic output of ${expected} for ${input}`, () => {
+        assert.equal(fizzbuzz(input, 'reduce'), expected);
+      });
+    };
+
+    for (var i = 1; i <= 21; i++) {
+      test(i);
+    }
+  });
+});
